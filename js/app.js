5683d32ac03bd0fcd18cc723944151727b558576
soote <waffleee@gmail.com>
2013-09-12 04:31:41 -0400

    cleanup

--- js/app.js
+++ js/app.js
@@ -1,58 +1,64 @@
-$( init );
- $(document).ready(function(){
- 	for(var i = Math.random()*100;i>0;i--){
- 		scramblePuzzle();
- 	}
+$(document).ready(function(){
+	$( init );
+	for(var i = Math.random()*100;i>0;i--){
+		swapTiles();
+	}
 })
+
 function init() {
 
 	var oldOffset = {x:0,y:0};
 
 	$('.draggable').draggable( {
 		containment: '#puzzle',
-    	cursor: 	'move',
-    	snap: 		'#row td',
-    	stack: 		'#row td',
-    	revert:     true, 
-    	start: 		onClick
+		cursor: 	 'move',
+		snap: 		 '#row td',
+		stack: 		 '#row td',
+		revert:    	 true, 
+		start: 		 onClick
 
  	});
 	
 	$('.draggable').droppable( {
-	  	accept: '#row td',
+	  	accept: 	'#row td',
 	  	hoverClass: 'hovered',
-	  	drop: onDrop
+	  	drop: 		onDrop
 	});
 	
 	function onDrop( event, ui ) {
+		//js bug in ecmascript, this points to global object (window) for inner functions
 		var that = this;
 
 		function validMove(){
 			var ydisplacement = Math.abs(oldOffset.y - $(that).context.offsetTop);
 			var xdisplacement = Math.abs(oldOffset.x - $(that).context.offsetLeft);
 
-			if( (ydisplacement===99 || xdisplacement===99) && 
-				ydisplacement !== xdisplacement  &&
-				$(that).context.id === "tileB" ) 
-					return true;
-			
-			else	return false;
+			//only moving 1 tile
+			var a = ydisplacement===99 || xdisplacement===99;
+			//vertical xor horizontal move
+			var b = ydisplacement===0  || xdisplacement===0;
+			//moving onto the blank tile
+			var c = $(that).context.id === "tile9";
+
+			return (a&&b&&c)?true:false;
 		}
+
 		if ( validMove() ) {
 		    ui.draggable.draggable( 'option', 'revert', false );
-		    ui.draggable.position( { of: $(that), my: 'left top', at: 'left top' } );
-	  	    $(that).offset({ top: oldOffset.y, left: oldOffset.x});
+		    ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+	  	    $(this).offset({ top: oldOffset.y, left: oldOffset.x});
 	  	}
 	}
+
 	function onClick( event ) {
-		oldOffset.y = $(this).context.offsetTop;
+	    oldOffset.y = $(this).context.offsetTop;
   	    oldOffset.x = $(this).context.offsetLeft;
 	    $(this).draggable( 'option', 'revert', true );
 
 	}
 }
 
-function scramblePuzzle(){
+function swapTiles(){
 	function getRandomInt (min, max) {
 	    return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
@@ -63,15 +69,10 @@ function scramblePuzzle(){
 	var pOne = $('#tile'+ firstTile);
 	var pTwo = $('#tile'+ secondTile);
 
+	//swap
 	var oldOffset = pOne.offset();
-	
-	// console.log( pOne[0].id,"- ",pOne.offset(),": ", pTwo[0].id,"- ",pTwo.offset());
-	// console.log( oldOffset );
-
 	pOne.offset( pTwo.offset() );
-	// console.log( oldOffset );
 	pTwo.offset( oldOffset );
 
-	// console.log( pOne[0].id,"- ",pOne.offset(),": ", pTwo[0].id,"- ",pTwo.offset());
 
 }
